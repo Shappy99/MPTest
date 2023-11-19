ea4f9cdc7944a6e19ca87d5f621f7707f1ecd873
Shappy99 <142829878+Shappy99@users.noreply.github.com>
2023-11-19 22:27:10 +0200

    a

---
+++ .ignore/mp_test.gd
@@ -0,0 +1,29 @@
+extends Node2D
+
+var peer = ENetMultiplayerPeer.new()
+@export var player_scene: PackedScene
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_host_pressed():
+	peer.create_server(135)
+	multiplayer.multiplayer_peer = peer
+	multiplayer.peer_connected.connect(_add_player)
+	_add_player()
+	$host.disabled = true
+func _add_player(id = 1):
+	var player = player_scene.instantiate()
+	player.name = str(id)
+	call_deferred("add_child", player)
+
+func _on_join_pressed():
+	peer.create_client("localhost", 135)
+	multiplayer.multiplayer_peer = peer
