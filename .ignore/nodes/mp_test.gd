2093dcfd0ac48b8321c84ad0de4b361b1170c9c4
Shappy99 <142829878+Shappy99@users.noreply.github.com>
2023-11-21 21:41:36 +0200

    ifeelstupid

---
+++ .ignore/nodes/mp_test.gd
@@ -0,0 +1,42 @@
+extends Node2D
+
+var peer = ENetMultiplayerPeer.new()
+@export var player_scene: PackedScene
+var IP_ADDRESS = 'localhost'#'86.123.205.95'
+var PORT = 25565
+var MAX_CLIENTS = 4
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	if multiplayer.is_server():
+		print_once_per_client.rpc()
+
+@rpc
+func print_once_per_client():
+	print("I will be printed to the console once per each connected client.")
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+func _on_host_pressed():
+	peer.create_server(PORT, MAX_CLIENTS)
+	multiplayer.multiplayer_peer = peer
+	multiplayer.peer_connected.connect(_add_player)
+	_add_player()
+	$host.visible = false
+	$host.disabled = true
+	$join.visible = false
+	$join.disabled = true
+	
+func _add_player(id = 1):
+	var player = player_scene.instantiate()
+	player.name = str(id)
+	call_deferred("add_child", player)
+
+func _on_join_pressed():
+	peer.create_client(IP_ADDRESS, PORT)
+	multiplayer.multiplayer_peer = peer
+	$host.visible = false
+	$host.disabled = true
+	$join.visible = false
+	$join.disabled = true
